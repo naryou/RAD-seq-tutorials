26c534879610c89e27645d8d34d27c24deaffe25
Narjes Yousefi <48094864+naryou@users.noreply.github.com>
2024-08-20 10:11:52 +0200

    Update bash_template.sh

--- stacks/bash_template.sh
+++ stacks/bash_template.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+src1=/home/ubuntu/course/
+src2=$src1/raw_data
 
-PATH_to_STACKS/stacks/process_radtags
-
-
+$src1/executables/hts_SeqScreener --singleend-input $src2/R1_001.fastq.gz \
+-L R1_001.phix.json -f R1_001.phiX 
